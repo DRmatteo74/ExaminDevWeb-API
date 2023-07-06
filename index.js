31c0551b15d7100b1d2b889610e5badc9e92edf7
Phoenix7405 <60186533+Phoenix7405@users.noreply.github.com>
2023-07-06 15:12:09 +0200

    Ajout de middleware express

--- index.js
+++ index.js
@@ -2,6 +2,22 @@
 // http permet de créer un serveur http
 const http = require('http');
 
+// Importation du module `express` depuis Node.js
+// express permet de créer des routes, des middlewares ...
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('Middleware 1!');
+    next(); // Permet à la requête de continuer vers le prochain middleware
+});
+
+app.use((req, res, next) => {
+    console.log('Middleware 2!');
+    res.send('<h1>Hello world !</h1>'); 
+});
+
 // Création du serveur HTTP
 const server = http.createServer();
 
